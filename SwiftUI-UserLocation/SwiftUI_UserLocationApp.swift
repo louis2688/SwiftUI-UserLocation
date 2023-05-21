c4abc768919b6446285977f14e43e331e72f1eaa
louis2688 <louismadrigal26@gmail.com>
2023-05-21 14:13:11 +0800

    Initial Commit

---
+++ SwiftUI-UserLocation/SwiftUI_UserLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UserLocationApp.swift
+//  SwiftUI-UserLocation
+//
+//  Created by dev on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UserLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
